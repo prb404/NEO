6d1a01d2e7908bbe70683e5f3922ac452f3fbca0
prb404 <64775485+prb404@users.noreply.github.com>
2022-10-21 20:56:59 +0200

    copy

---
+++ neo/callingJsCssNEO - Copy.js
@@ -0,0 +1,27 @@
+function loadjscssfileneo(filename, filetype){
+ if (filetype=="js"){ //if filename is a external JavaScript file
+  var fileJs=document.createElement('script')
+  fileJs.setAttribute("type","text/javascript")
+  fileJs.setAttribute("src", filename)
+ }
+ else if (filetype=="css"){ //if filename is an external CSS file
+  var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", filename)
+ }
+ if (typeof fileref!="undefined"){
+  document.getElementsByTagName("head")[0].appendChild(fileref)
+ }
+ else if (typeof fileJs!="undefined"){
+  document.getElementsByTagName("body")[0].appendChild(fileJs)
+ }
+}
+
+// external JS
+//loadjscssfileneo("https://url.com/.../customJavaScript.js", "js") //dynamically load and add this .js file
+loadjscssfileneo("https://cdn.logwork.com/widget/countdown.js", "js")
+
+// external CSS
+//loadjscssfileneo("https://url.com/.../customStyleSheet.css", "css") ////dynamically load and add this .css file
+loadjscssfileneo("https://prb404.github.io/NEO/neo/customStyleSheetNEO - Copy.css", "css")
